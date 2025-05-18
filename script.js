458a27e1d1be4b0c9c1728cf3c2c09cfbcb36235
elielbarbosa2 <elielbarbosa82@gmail.com>
2025-05-18 15:36:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.innerText = "Bem-vindo à minha página! Estou construindo e vou continuar estudando para melhorar meus projetos, obrigado por estar aqui.";
+  }
